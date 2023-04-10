e481c7e5b1612680c65ffb7f6bef59fd641aa118
8490 <ethan@clarusway.com>
2023-04-10 13:23:01 +0300

    add login fields

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,12 +6,31 @@ import Typography from "@mui/material/Typography";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
 import { Link, useNavigate } from "react-router-dom";
-
 import { useSelector } from "react-redux";
+import { Formik, Form } from "formik";
+import TextField from "@mui/material/TextField";
+import { object, string, number, date, InferType } from "yup";
 
 const Login = () => {
   const navigate = useNavigate();
   const { currentUser, error } = useSelector((state) => state?.auth);
+
+  const loginScheme = object({
+    email: string()
+      .email("Lütfen geçerli bir mail giriniz")
+      .required("Bu alan zorunludur"),
+    password: string()
+      .required("Password zorunludur")
+      .min(8, "Password en az 8 karakter olmalıdır")
+      .max(16, "Password en fazla 16 karakter olmalıdır")
+      .matches(/\d+/, "Password sayı içermelidir")
+      .matches(/[a-z]/, "Password bir küçük harf içermelidir")
+      .matches(/[A-Z]/, "Password bir büyük harf içermelidir")
+      .matches(
+        /[$&+,:;=?@#|'<>.-^*()%!]+/,
+        "Password bir özel karakter içermelidir"
+      ),
+  });
 
   return (
     <Container maxWidth="lg">
@@ -49,6 +68,48 @@ const Login = () => {
           >
             Login
           </Typography>
+
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginScheme}
+            onSubmit={(values, actions) => {
+              //TODO  login with values   POST  req,
+              //navigate
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+          >
+            {({ values, handleChange, handleBlur, errors, touched }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    value={values?.email || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <TextField
+                    label="Password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    value={values?.password || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+                </Box>
+              </Form>
+            )}
+          </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
