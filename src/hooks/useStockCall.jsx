cc7bad4b5640122fefe52bfd00cfb6bd6b9f2583
8490 <ethan@clarusway.com>
2023-05-22 16:20:19 +0300

    add delete data with axios instance

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -2,18 +2,17 @@ import axios from "axios";
 
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useAxios from "./useAxios";
+
 const useStockCall = () => {
   const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const getStockData = async (url) => {
-    const BASE_URL = "http://10001.fullstack.clarusway.com/";
+  const { axiosWithToken } = useAxios();
 
+  const getStockData = async (url) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axios(`${BASE_URL}stock/${url}/`, {
-        headers: { Authorization: `Token ${token}` },
-      });
-      // dispatch(getSuccess(data));
+      const { data } = await axiosWithToken(`stock/${url}/`);
       dispatch(getSuccess({ data, url }));
     } catch (error) {
       console.log(error);
@@ -21,19 +20,45 @@ const useStockCall = () => {
     }
   };
   const deleteStockData = async (url, id) => {
-    const BASE_URL = "http://10001.fullstack.clarusway.com/";
-
     dispatch(fetchStart());
     try {
-      await axios.delete(`${BASE_URL}stock/${url}/${id}/`, {
-        headers: { Authorization: `Token ${token}` },
-      });
+      await axiosWithToken.delete(`stock/${url}/${id}/`);
       getStockData(url);
     } catch (error) {
       console.log(error);
       dispatch(fetchFail());
     }
   };
+
+  //   const getStockData = async (url) => {
+  //     const BASE_URL = "http://10001.fullstack.clarusway.com/";
+
+  //     dispatch(fetchStart());
+  //     try {
+  //       const { data } = await axios(`${BASE_URL}stock/${url}/`, {
+  //         headers: { Authorization: `Token ${token}` },
+  //       });
+  //       // dispatch(getSuccess(data));
+  //       dispatch(getSuccess({ data, url }));
+  //     } catch (error) {
+  //       console.log(error);
+  //       dispatch(fetchFail());
+  //     }
+  //   };
+  //   const deleteStockData = async (url, id) => {
+  //     const BASE_URL = "http://10001.fullstack.clarusway.com/";
+
+  //     dispatch(fetchStart());
+  //     try {
+  //       await axios.delete(`${BASE_URL}stock/${url}/${id}/`, {
+  //         headers: { Authorization: `Token ${token}` },
+  //       });
+  //       getStockData(url);
+  //     } catch (error) {
+  //       console.log(error);
+  //       dispatch(fetchFail());
+  //     }
+  //   };
 
   return { getStockData, deleteStockData };
 };
