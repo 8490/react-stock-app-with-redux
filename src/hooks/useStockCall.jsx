e2423452f2a36dbc687d9503f1a17e01ff76e9bd
8490 <ethan@clarusway.com>
2023-05-22 12:32:36 +0300

    add useStockData custom hook

---
+++ src/hooks/useStockCall.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+
+const useStockCall = () => {
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const getStockData = async (url) => {
+    const BASE_URL = "http://10001.fullstack.clarusway.com/";
+
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios(`${BASE_URL}stock/${url}/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      // dispatch(getSuccess(data));
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+  return { getStockData };
+};
+
+export default useStockCall;
