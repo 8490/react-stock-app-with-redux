0ea447d1751f638098acc8cbd16433613362414f
8490 <ethan@clarusway.com>
2023-05-22 12:32:50 +0300

    add custom hook

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,31 +1,33 @@
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useStockCall from "../hooks/useStockCall";
+// import axios from "axios";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const { token } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const getFirms = async () => {
-    const BASE_URL = "http://10001.fullstack.clarusway.com/";
+  // const { token } = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
+  // const getFirms = async () => {
+  //   const BASE_URL = "http://10001.fullstack.clarusway.com/";
 
-    dispatch(fetchStart());
-    const url = "firms";
-    try {
-      const { data } = await axios(`${BASE_URL}stock/firms/`, {
-        headers: { Authorization: `Token ${token}` },
-      });
-      // dispatch(getSuccess(data));
-      dispatch(getSuccess({ data, url }));
-    } catch (error) {
-      console.log(error);
-      dispatch(fetchFail());
-    }
-  };
+  //   dispatch(fetchStart());
+  //   const url = "firms";
+  //   try {
+  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     });
+  //     // dispatch(getSuccess(data));
+  //     dispatch(getSuccess({ data, url }));
+  //   } catch (error) {
+  //     console.log(error);
+  //     dispatch(fetchFail());
+  //   }
+  // };
+  const { getStockData } = useStockCall();
   useEffect(() => {
-    getFirms();
+    getStockData("firms");
   }, []);
   return (
     <div>
