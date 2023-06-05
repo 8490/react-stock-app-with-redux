f332452be86d8abbd23f5e43af71608b585a6028
8490 <ethan@clarusway.com>
2023-06-05 15:37:44 +0300

    create product pages

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,57 @@
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
+import { Grid } from "@mui/material";
+import FirmCard from "../components/FirmCard";
+import { flex } from "../styles/globalStyles";
+import FirmModal from "../components/modals/FirmModal";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const { getStockData } = useStockCall();
+  const { firms } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
 
-export default Products
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    getStockData("firms");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(firms);
+  return (
+    <div>
+      <Typography variant="h4" color="error" sx={{ mb: 2 }}>
+        Firms
+      </Typography>
+      <Button variant="contained" sx={{ mb: 2 }} onClick={handleOpen}>
+        New Firm
+      </Button>
+
+      <FirmModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+      <Grid container sx={flex}>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} setOpen={setOpen} setInfo={setInfo} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Products;
