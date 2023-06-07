c6822ee1df7ea0061db2a153cf5aa6114897d305
8490 <ethan@clarusway.com>
2023-06-07 11:22:47 +0300

    add product table

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,6 +7,13 @@ import { Grid } from "@mui/material";
 import { flex } from "../styles/globalStyles";
 import ProductModal from "../components/modals/ProductModal";
 import ProductCard from "../components/ProductCard";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Products = () => {
   const { getStockData } = useStockCall();
@@ -43,7 +50,38 @@ const Products = () => {
         info={info}
         setInfo={setInfo}
       />
-      <Grid container sx={flex}>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Brand</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, index) => (
+              <TableRow
+                key={product.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {index + 1}
+                </TableCell>
+                <TableCell align="right">{product.calories}</TableCell>
+                <TableCell align="right">{product.fat}</TableCell>
+                <TableCell align="right">{product.carbs}</TableCell>
+                <TableCell align="right">{product.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* <Grid container sx={flex}>
         {products?.map((product) => (
           <Grid item key={product.id}>
             <ProductCard
@@ -53,7 +91,7 @@ const Products = () => {
             />
           </Grid>
         ))}
-      </Grid>
+      </Grid> */}
     </div>
   );
 };
