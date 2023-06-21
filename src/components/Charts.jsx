2ea35361a806cf3116b17191f23c2ba66b961b1c
8490 <ethan@clarusway.com>
2023-06-21 15:41:14 +0300

    add data for charts

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,5 +1,29 @@
+import { Card, Title, LineChart } from "@tremor/react";
+import { useSelector } from "react-redux";
+
+const dataFormatter = (number) =>
+  `${Intl.NumberFormat("us").format(number).toString()}%`;
+
 const Charts = () => {
-  return <div>Charts</div>;
+  const { sales, purchases } = useSelector((state) => state.stock);
+  const salesData = sales.map((item) => ({
+    date: item.createds,
+    sale: item.price_total,
+  }));
+  return (
+    <Card className="mt-4">
+      <Title>Total Sales</Title>
+      <LineChart
+        className="mt-6"
+        data={""}
+        index="date"
+        categories={["Export Growth Rate", "Import Growth Rate"]}
+        colors={["emerald", "gray"]}
+        valueFormatter={dataFormatter}
+        yAxisWidth={40}
+      />
+    </Card>
+  );
 };
 
 export default Charts;
